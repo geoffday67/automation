6f5842d5f1ed6d4cea4e5ce98b694dec718d69ca
Geoff Day <geoffday67@gmail.com>
2017-08-21 22:37:21 +0100

    Change to URL encoded

--- automation.js
+++ automation.js
@@ -4,7 +4,7 @@ var bodyParser = require('body-parser')
 var app = express ();
 var server = require ('http').createServer (app);
 
-app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({extended: false}))
 app.use (express.static (__dirname + '/'));
 
 var handlers = [];
